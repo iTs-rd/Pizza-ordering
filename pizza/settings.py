b5ac871dafa3c5df64c4768d7c66fd78a188498a
rudresh gupta <rudreshgpt7@gmail.com>
2022-02-08 18:50:59 +0530

    add static files

--- pizza/settings.py
+++ pizza/settings.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -117,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
+MEDIA_URL= "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
